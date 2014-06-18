46228a19b99294f28879c00b7eb1bc9851bcf1ad
Alexis Vanacker <alexvanacker@gmail.com>
2014-06-18 22:36:25 +0200

    feat(scrap): get beer style links
    Add function that returns dict of link to (beer style, global beer type) tuple

--- miniscrapper.py
+++ miniscrapper.py
@@ -4,9 +4,52 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://www.beeradvocate.com/beer/profile/27039/16814/')
 
-contents = r.content
-soup = BeautifulSoup(contents)
-print soup.title
+def get_styles_url_and_names():
+    '''
+    Returns a dict linking an url to a beer style and its global type
+    '''
 
+    root_page = 'http://www.beeradvocate.com'
+    current_page = 'http://www.beeradvocate.com/beer/style/'
+    r = requests.get(current_page)
+
+    contents = r.content
+    soup = BeautifulSoup(contents)
+
+    body = soup.body
+    table = body.table
+    first_tr = table.tr
+    global_list = {}
+    for td in first_tr.find_all('td', recursive=False):
+        for td_table in td.find_all('table', recursive=False):
+            tr_in_table = td_table.find_all('tr', limit=2)
+            # Contains all names and links to beer styles
+            style_list_html = tr_in_table[1].td
+            for style_name_html in style_list_html.find_all('b'):
+                style_name = style_name_html.contents[0]
+                # Go down in the tr tree, until you have a double
+                # break line. Such ugly.
+                double_br = False
+                previous_was_br = False
+                previous_element = style_name_html
+                while not double_br:
+                    next_element = previous_element.find_next()
+                    is_br = next_element.name.find('br') > -1
+                    double_br = is_br and previous_was_br
+
+                    # Now, if it's a link, add it to the dict
+                    if next_element.name == 'a':
+                        link = next_element['href']
+                        full_link = root_page + link
+                        beer_style_name = next_element.contents[0]
+                        global_list[full_link] = (beer_style_name, style_name)
+
+                    previous_was_br = is_br
+                    previous_element = next_element
+
+    return global_list
+
+if __name__ == '__main__':
+    ulrs_tuples = get_styles_url_and_names()
+    print str(ulrs_tuples)

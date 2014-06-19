9d2bb6af35b47fde68c0564143350040759edfa8
Alexis Vanacker <alexvanacker@gmail.com>
2014-06-19 20:59:04 +0200

    fix(count reviews): handle ',' in numbers
    American way of writing numbers is used on site, meaning that
    1230 is written '1,230'. Fix count of reviews and comments
    based on this.
    Add try/except in count method for easier debugging (give beer name
    and text extracted).
    Some formatting.
    Add timing utility for measuring processing time. Estimated at an
    average 1 sec/beer page : a bit slow, so need to optimize!

--- miniscrapper.py
+++ miniscrapper.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import time
 import re
 from bs4 import BeautifulSoup
 
@@ -59,7 +60,7 @@ def make_soup(url):
     r = requests.get(url)
     # FIXME test if response code is OK!
     contents = r.content
-    soup = BeautifulSoup(contents, "html5lib") #HTML5 cuz this site be trippin'
+    soup = BeautifulSoup(contents, 'html5lib')  # HTML5 cuz this site be trippin'
     return soup
 
 
@@ -94,7 +95,7 @@ def get_all_beers_from_substyle(substyle_url):
     root_url = 'http://www.beeradvocate.com/'
     url_list = []
     substyle_urls = get_substyle_url(substyle_url) # Get list of all pages for the substyle which contains all beers!
-    
+
     for page_url in substyle_urls:
         soup = make_soup(page_url)
         links = soup.find_all(href=re.compile('beer/profile/(.+)/(.+)/$'))
@@ -125,7 +126,7 @@ def get_beer_stats(profile_page_soup):
     stats_tr = main_div.table.tr.find_all('td')[1].find('tr')
     real_stats_tr = stats_tr.find('tr')
     stats_td_list = real_stats_tr.find_all('td', recursive=False)
-    
+
     if len(stats_td_list) < 3:
         print 'Error on profile page, not enough columns'
     # first td is BA score
@@ -146,9 +147,9 @@ def get_beer_stats(profile_page_soup):
     td_text = summary_td.get_text()
     match_nb_ratings = re.search('Ratings: (\d+)?', td_text)
     if match_nb_ratings:
-        nb_ratings =  match_nb_ratings.group(1)
+        nb_ratings = match_nb_ratings.group(1)
         infos_dict['nb_ratings'] = nb_ratings
-    
+
     match_nb_reviews = re.search('Reviews: (\d+)?', td_text)
     if match_nb_reviews:
         nb_reviews = match_nb_reviews.group(1)
@@ -189,31 +190,45 @@ def count_number_of_beers():
     print 'Total number of beers: '+str(total)
     return total
 
+
 def count_number_of_ratings_and_comments_for_beer_url(beer_url):
     '''
-    Returns a tuple (nb_ratings, nb_comments), representing the 
+    Returns a tuple (nb_ratings, nb_comments), representing the
     number of ratings and comments for a given beer page.
     '''
     soup = make_soup(beer_url)
     ba_content = soup.find(id='baContent')
-    divs = ba_content.find_all('div',recursive=False, limit=6)
+    divs = ba_content.find_all('div', recursive=False, limit=6)
     last_div = divs[5]
     text = last_div.get_text()
     nb_ratings = 0
     nb_reviews = 0
 
     # now parse it
-    match_nb_ratings = re.search('Ratings: (\d+)?', text)
-    if match_nb_ratings:
-        nb_ratings = int(match_nb_ratings.group(1))
+    # WARNING: Ratings and Reviews use ',' for thousands!
+    match_nb_ratings = re.search('Ratings: (\d+,?\d*)?', text)
+    match_nb_reviews = re.search('Reviews: (\d+,?\d*)?', text)
+    beer_name = get_beer_name(soup)
+    print 'Counting for ' + beer_name
+    try:
+        if match_nb_ratings:
+            nb_ratings = int(match_nb_ratings.group(1).replace(',', ''))
 
-    match_nb_reviews = re.search('Reviews: (\d+)?', text)
-    if match_nb_reviews:
-        nb_reviews = int(match_nb_reviews.group(1))
-    print (nb_ratings, nb_reviews)
-    return (nb_ratings, nb_reviews)
-    #print soup.prettify()
-    
+        if match_nb_reviews:
+            nb_reviews = int(match_nb_reviews.group(1).replace(',', ''))
+
+        if nb_ratings < nb_reviews:
+            print 'More ratings than reviews: '+beer_name
+
+        print (nb_ratings, nb_reviews)
+        return (nb_ratings, nb_reviews)
+
+    except Exception:
+        print 'Error on beer: ' + beer_name
+        print 'text: '+text
+        print match_nb_ratings.group(1)
+        print match_nb_reviews.group(1)
+        raise
 
 
 def count_number_of_ratings_and_comments():
@@ -223,20 +238,31 @@ def count_number_of_ratings_and_comments():
     '''
     dict_url_styles = get_styles_url_and_names()
     total_reviews = 0
-    total_ratings = 0 
+    total_ratings = 0
+    total_time = 0
+    count = 0
     for url in dict_url_styles.keys():
         beer_urls = get_all_beers_from_substyle(url)
         for beer_url in beer_urls:
+            count += 1
+            start = time.time()
             (nb_ratings, nb_reviews) = count_number_of_ratings_and_comments_for_beer_url(beer_url)
+            end = time.time()
+            beer_time = end - start
+            total_time += beer_time
+            if count % 10 == 0:
+                avg_time = total_time / 10
+                total_time = 0
+                print 'Average time per beer: ' + str(avg_time)
             total_ratings += nb_ratings
             total_reviews += nb_reviews
     return (total_ratings, total_reviews)
 
 
-
-
 if __name__ == '__main__':
     #count_number_of_beers()
     count_number_of_ratings_and_comments()
-    #count_number_of_ratings_and_comments_for_beer_url('http://www.beeradvocate.com/beer/profile/302/73119/')
+
+    # Keep for Tests!
+    # count_number_of_ratings_and_comments_for_beer_url('http://www.beeradvocate.com/beer/profile/26520/115426/')
     #parse_beer_profile('http://www.beeradvocate.com/beer/profile/16315/61128/')

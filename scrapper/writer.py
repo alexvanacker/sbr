2968092d43516aaeb8c0abb184ac150b4fd2316d
Alexis Vanacker <alexvanacker@gmail.com>
2014-07-16 17:21:22 +0200

    refactor(writer): finish refactoring
    Add handling of lists coming from scrapper functions in global writer.
    Adapt review writer.

--- scrapper/writer.py
+++ scrapper/writer.py
@@ -30,7 +30,11 @@ def global_writer(list_url, dest_file_path, scrapper_function,
                     print 'Could not find one URL that could be reached.'
                     raise
 
-        field_names = sample_infos.keys()
+        if isinstance(sample_infos, list):
+            field_names = sample_infos[0].keys()
+        else:
+            field_names = sample_infos.keys()
+
         csv_writer = csv.DictWriter(dest_file, fieldnames=field_names)
         csv_writer.writeheader()
 
@@ -61,7 +65,10 @@ def global_writer(list_url, dest_file_path, scrapper_function,
                     print 'Moving on to the next.'
 
             if info:
-                temp_array.append(info)
+                if isinstance(info, list):
+                    temp_array.extend(info)
+                else:
+                    temp_array.append(info)
                 current_processed += 1
                 if write_every_nbr > 0 and current_processed >= write_every_nbr:
 
@@ -127,39 +134,26 @@ def write_all_beers_reviews(list_url, dest_file_path, write_every_nbr=0,
     size_limit -- Size limit for the file in Mb (default = 0)
     compress -- Write the CSV file as a compressed file (default = True)
     """
-    # Prepare CSV writer
-    dest_file = open(dest_file_path, 'wb')
-    try:
-        print 'Writing review data to ' + dest_file_path
 
-        # get the first url and fetch its info to create the csv header
-        found_good_url = False
-        index = 0
-        nb_beers = len(list_url)
-        while not found_good_url:
-            if index > nb_beers - 1:
-                raise Exception('Could not find one URL that could be reached.')
-            try:
-                url = list_url[index]
-                sample_infos = scrapper.extract_reviews_from_url(url)[0]
-                found_good_url = True
-            except:
-                index += 1
+    # Get the list of all review URLs
+    reviews_urls = []
 
-        field_names = sample_infos.keys()
-        csv_writer = csv.DictWriter(dest_file, fieldnames=field_names)
-        csv_writer.writeheader()
+    for beer_url in list_url:
+        last_page = scrapper.find_last_number_of_subpages_from_url(beer_url)
+        start = 0
+        last_page_int = int(last_page)
 
-        for beer_url in list_url:
-            write_beer_reviews(beer_url, csv_writer, write_every_nbr,
-                               size_limit)
+        # Get list of review URLs for the beer
+        while start <= last_page_int:
+            url_ratings = beer_url + '?hideRatings=N&start=' + str(start)
+            reviews_urls.append(url_ratings)
+            # Reviews are 25 by 25
+            start = start + 25
 
-    except:
-        print 'Global error while writing reviews to ' + dest_file_path
-        raise
-
-    finally:
-        dest_file.close()
+    # Now we write
+    global_writer(reviews_urls, dest_file_path,
+                  scrapper.extract_reviews_from_url, write_every_nbr,
+                  size_limit, compress)
 
 
 def is_file_above_limit(filepath, limit=0):
@@ -181,72 +175,6 @@ def is_file_above_limit(filepath, limit=0):
         return True
     else:
         return False
-
-
-def write_beer_reviews(beer_url, csv_writer, write_every_nbr=0,
-                       size_limit=0):
-    """ Writes a beer's reviews to csv file
-
-    beer_url -- beer profile URL
-    csv_writer -- CSV writer for the csv file
-    write_every_nbr -- number of reviews to store before writing to file
-    (default = 0)
-    size_limit -- Size limit for the file in Mb (default = 0)
-    """
-    reviews = []
-    error_list = []
-
-    # First make sure we have access to the url
-    scrapper.test_access_url(beer_url)
-
-    # Get list of subpages
-    try:
-        last_page = scrapper.find_last_number_of_subpages_from_url(beer_url)
-        reviews_urls = []
-        start = 0
-        last_page_int = int(last_page)
-
-        # Get list of review URLs
-        while start <= last_page_int:
-            url_ratings = beer_url + '?hideRatings=N&start=' + str(start)
-            reviews_urls.append(url_ratings)
-            # Reviews are 25 by 25
-            start = start + 25
-
-        for url in reviews_urls:
-            beer_reviews = None
-
-            try:
-                beer_reviews = scrapper.extract_reviews_from_url(url)
-            except:
-                print 'Error while loading URL: ' + url
-                print 'Trying again in 5 seconds...'
-                time.sleep(5)
-
-                try:
-                    beer_reviews = scrapper.extract_reviews_from_url(url)
-                except Exception, e:
-                    error_list.append(url)
-                    print 'Could not load URL: ' + beer_url
-                    print str(e)
-                    print 'Moving on to the next.'
-
-            if beer_reviews:
-                reviews.extend(beer_reviews)
-
-                if write_every_nbr > 0 and len(reviews) > write_every_nbr:
-                    write_unicode_csv_rows(reviews, csv_writer)
-
-                    # Reset
-                    reviews = []
-
-        # Write remaining reviews
-        if len(reviews) > 0:
-            write_unicode_csv_rows(reviews, csv_writer)
-
-    except:
-        print 'Error writing reviews from beer URL: ' + beer_url
-        raise
 
 
 def write_unicode_csv_rows(dicts, csv_writer):

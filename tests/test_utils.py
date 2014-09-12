0b3c824975cd2dc934093d428b32f53355ae4d8e
Alexis Vanacker <alexvanacker@gmail.com>
2014-09-12 14:13:08 +0200

    tests(utils): UTs for date utils
    Because man... Tests man...

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,7 +15,36 @@ class UtilsTest(unittest.TestCase):
         assert date == expected
 
 
+    def test_yesterday(self):
+        s = 'Yesterday at 12:34 PM'
+        now = datetime.datetime.now()
+        expected = now - datetime.timedelta(days=1)
+        expected = expected.replace(hour=12, minute=34, microsecond=0, second=0)
+        date = utils.extract_date(s)
+        assert date == expected
+
+
     def test_time_ago(self):
-        s = '51 minutes ago'
+        diff = 51
+        s = str(diff)+' minutes ago'
+        now = datetime.datetime.now()
+        expected = now - datetime.timedelta(minutes=diff)
+        expected = expected.replace(second=0, microsecond=0)
+        result = utils.extract_date(s)
+        assert expected == result
+
+    def test_minute_ago(self):
+        s = '1 minute ago'
+        now = datetime.datetime.now()
+        expected = now - datetime.timedelta(minutes=1)
+        expected = expected.replace(second=0, microsecond=0)
+        result = utils.extract_date(s)
+        assert expected == result
+
+    def test_date(self):
+        s = 'Mar 16, 1987'
+        expected = datetime.datetime.now().replace(month=3, year=1987, day=16, hour=0, minute=0, second=0, microsecond=0)
+        result = utils.extract_date(s)
+        assert expected == result
 
 

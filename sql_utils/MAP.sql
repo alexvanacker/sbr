10d7d7664fc29f2032b6c52bafa6bf8bf181b36e
Pierre G <pierre.gutierrez@dataiku.com>
2014-10-02 12:03:19 +0200

    sql_utils MAP

---
+++ sql_utils/MAP.sql
@@ -0,0 +1,28 @@
+/*
+
+MAP calculation in sql :
+réf : https://www.kaggle.com/wiki/MeanAveragePrecision
+
+- n main parameter
+- has bought = 1 if user bought the product else 0
+- row number = predicted rank. No ties. 
+
+- comm : argh the pb is that I have 
+
+*/
+
+
+SELECT AVG(MAP_n) as MAP_n
+FROM (
+    SELECT memberid
+        , sum((row_number <= m and row_number <= n)::int * hasbought/row_number)/ LEAST(m,n) as MAP_n
+    FROM (
+    	SELECT memberid 
+    		, has_bought
+    		, row_number
+    		, sum(hasbought) as m 
+    		, count(*) as nb_tot
+    	FROM MYTABLE ) s1
+    WHERE row_number <= m and row_number <= n 
+    GROUP BY memberid ) s2
+; 

0143e69461a82e32f3cc3e044e8d781ab7fac37b
Pierre G <pierre.gutierrez@dataiku.com>
2014-10-02 17:34:37 +0200

    sql utils real map working

--- sql_utils/MAP.sql
+++ sql_utils/MAP.sql
@@ -7,21 +7,23 @@ réf : https://www.kaggle.com/wiki/MeanAveragePrecision
 - has bought = 1 if user bought the product else 0
 - row number = predicted rank. No ties. 
 
-- comm : argh the pb is that I have 
+- demo of random is easy, just consider all permutation and everything got same chance to be at any place => bouyah 
 
 */
 
 
 SELECT AVG(MAP_n) as MAP_n
+	, AVG(MAP_n_random) as MAP_n_random
 FROM (
     SELECT memberid
-        , sum((row_number <= m and row_number <= n)::int * hasbought/row_number)/ LEAST(m,n) as MAP_n
+        , sum((row_number <= m and row_number <= n)::int * hasbought/row_number)/ LEAST(max(m),n) as MAP_n
+        , sum((row_number <= m and row_number <= n)::int * /row_number)/ count(*) as MAP_n_random
     FROM (
     	SELECT memberid 
     		, has_bought
     		, row_number
     		, sum(hasbought) as m 
-    		, count(*) as nb_tot
+    		-- , count(*) as nb_tot
     	FROM MYTABLE ) s1
     GROUP BY memberid ) s2
 ; 
